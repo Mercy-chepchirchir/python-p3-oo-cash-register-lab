0a30d41cad3d71f29d712a1c4df256d95153d741
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-24 19:44:32 +0300

    completed lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,46 +1,43 @@
-#!/usr/bin/env python3
-
 class CashRegister:
     def __init__(self, discount=0):
         self.discount = discount
         self.total = 0
         self.items = []
+        self.prices = []
 
     def add_item(self, title, price, quantity=1):
         self.total += price * quantity
+        self.prices.append(price)
         self.items.extend([title] * quantity)
 
     def apply_discount(self):
         if self.discount > 0:
-            self.total *= (1 - self.discount / 100)
-            self.total = round(self.total, 2)
-            return f"After the discount, the total comes to ${self.total:.2f}."
-        else:
-            return "There is no discount to apply."
+            discount_amount = self.total * self.discount / 100
+            rounded_discount = round(discount_amount)
+            self.total -= rounded_discount
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:    
+            print("There is no discount to apply.")
+
+    def get_all_items(self):
+        return self.items
 
     def void_last_transaction(self):
         if self.items:
             last_item_price = self._get_last_item_price()
             self.total -= last_item_price
             self.items.pop()
+        else:
+            print("No items to void.")
+            self.total = 0.0  # Reset the total to 0 if no items are left
 
     def _get_last_item_price(self):
         last_item = self.items[-1]
-        # You should have a way to look up the price of an item based on its title.
-        # For simplicity, let's assume there's a function named `get_price_for_item`
-        # that takes an item title and returns its price.
-        return get_price_for_item(last_item)
-
-    def _get_last_item_price(self):
-        last_item = self.items[-1]
-        # This is a placeholder method for demonstration purposes.
-        # You need to replace it with a proper method to get item prices.
+        # Use the item_prices dictionary to look up the price of the last_item.
         return self.get_price_for_item(last_item)
 
     def get_price_for_item(self, item_title):
-        # You should implement a logic here to fetch the price for the given item title.
-        # This could involve looking up the price in a database or a predefined dictionary.
-        # For this example, let's assume there's a dictionary named `item_prices`:
+        # For demonstration purposes, we'll use a dictionary to store item prices.
         item_prices = {
             "eggs": 0.98,
             "book": 5.00,
@@ -49,6 +46,6 @@ class CashRegister:
             # ... add more items and prices as needed ...
         }
 
-        # Return the price for the item_title if it exists in the dictionary.
-        # You should handle cases where the item is not found and provide an appropriate default behavior.
+        # Use the dictionary's get() method to retrieve the price for the given item_title.
+        # Provide a default value of 0.0 if the item_title is not found in the dictionary.
         return item_prices.get(item_title, 0.0)
